7d2ea20787cb2efd82c5c2c9e2460c85c7cbd8c6
Erin Wise <erinwise88@gmail.com>
2023-10-03 17:31:56 -0400

    complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,18 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linksElement = links.map((link) => {
+    console.log(link)
+    const href = `#${link}`
+    return <a key={link} href={href}>{link}</a>
+   });
+    return (
+      <nav>
+         {linksElement} 
+      </nav>);
+
+
+  
 }
 
 export default NavBar;
